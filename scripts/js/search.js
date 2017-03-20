b035fd0221ba1faec3ec39307759bb403272c2ed
Phoenixcon <krislegg2@gmail.com>
2017-03-20 15:45:21 -0500

    First commit

---
+++ scripts/js/search.js
@@ -0,0 +1,27 @@
+$('form').submit(function(event) {
+
+    event.preventDefault();
+
+    var mykey = config.themoviedb;
+    var movietitle = document.getElementById('search').value;
+    var baseapiurl = 'https://api.themoviedb.org/3/search/movie?api_key='+mykey+'&language=en-US&page=1&include_adult=false';
+    var apisearch = '&query='+movietitle;
+    var apiurl = baseapiurl+apisearch;
+
+    $.get( apiurl, function( data ) {
+        
+        var index;
+        var resultsnumber = data.total_results;
+
+        $("#results").empty();
+
+        for (index = 0; index < resultsnumber; ++index) {
+
+            if (data.results[index].poster_path == null) {
+                $("#results").append("<li><h2>"+data.results[index].title+"</h2><div class='noimage'></div></li>");
+            } else {
+                $("#results").append("<li><h2>"+data.results[index].title+"</h2><img src=http://image.tmdb.org/t/p/w185/"+data.results[index].poster_path+"></li>");
+            }}
+
+        }, "json" );
+    });
